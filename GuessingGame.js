ef183711aa877dad3665b756bdec8e7cdda79ef9
Destin McMurry <destinmcmurry@gmail.com>
2018-02-25 22:55:54 -0500

    updated

--- GuessingGame.js
+++ GuessingGame.js
@@ -46,25 +46,45 @@ Game.prototype.playersGuessSubmission = function(guess) {
   return this.checkGuess(guess);
 }
 
-Game.prototype.checkGuess = function(guess) {
-  if (guess === this.winningNumber) {
-    return 'You Win!';
-  } else if (this.pastGuesses.includes(guess)) {
-    return 'You have already guessed that number.';
-  }
-  this.pastGuesses.push(guess);
-  if (this.pastGuesses.length >= 5) {
-    return 'You Lose.';
-  }
-  if (Math.abs(guess - this.winningNumber) < 10) {
-    return 'You\'re burning up!';
-  } else if (Math.abs(guess - this.winningNumber) < 25) {
-    return 'You\'re lukewarm.';
-  } else if (Math.abs(guess - this.winningNumber) < 50) {
-    return 'You\'re a bit chilly.';
-  } else if (Math.abs(guess - this.winningNumber) < 100) {
-    return 'You\'re ice cold!';
-  }
+Game.prototype.checkGuess = function() {
+    if(this.playersGuess===this.winningNumber) {
+        $('#hint, #submit').prop("disabled",true);
+        $('#subtitle').text("Press the Reset button to play again!")
+        return 'You Win!'
+    }
+    else {
+        if(this.pastGuesses.indexOf(this.playersGuess) &gt; -1) {
+            return 'You have already guessed that number.';
+        }
+        else {
+            this.pastGuesses.push(this.playersGuess);
+            $('#guess-list li:nth-child('+ this.pastGuesses.length +')').text(this.playersGuess);
+            if(this.pastGuesses.length === 5) {
+                $('#hint, #submit').prop("disabled",true);
+                $('#subtitle').text("Press the Reset button to play again!")
+                return 'You Lose.';
+            }
+            else {
+                var diff = this.difference();
+                if(this.isLower()) {
+                    $('#subtitle').text("Guess Higher!")
+                } else {
+                    $('#subtitle').text("Guess Lower!")
+                }
+                if(diff &lt; 10) return'You\'re burning up!';
+                else if(diff &lt; 25) return'You\'re lukewarm.';
+                else if(diff &lt; 50) return'You\'re a bit chilly.';
+                else return'You\'re ice cold!';
+            }
+        }
+    }
+}
+
+function makeAGuess(game) {
+    var guess = $('#player-input').val();
+    $('#player-input').val("");
+    var output = game.playersGuessSubmission(parseInt(guess,10));
+    $('#title').text(output);
 }
 
 newGame = function() {
@@ -78,3 +98,30 @@ Game.prototype.provideHint = function() {
   }
   return shuffle(nums);
 }
+
+$(document).ready(function() {
+    var game = new Game();
+
+    $('#submit').click(function(e) {
+       makeAGuess(game);
+    })
+
+    $('#player-input').keypress(function(event) {
+        if ( event.which == 13 ) {
+           makeAGuess(game);
+        }
+    })
+})
+
+$('#hint').click(function() {
+    var hints = game.provideHint();
+    $('#title').text('The winning number is '+hints[0]+', '+hints[1]+', or '+hints[2]);
+});
+
+$('#reset').click(function() {
+    game = newGame();
+    $('#title').text('Play the Guessing Game!');
+    $('#subtitle').text('Guess a number between 1-100!')
+    $('.guess').text('-');
+    $('#hint, #submit').prop("disabled",false);
+})
